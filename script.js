d18dcda2c18e2653963c16b6c3daef32eeec9d3d
cesu19 <cesarhernancorbalan19@gmail.com>
2024-02-03 20:26:00 -0300

    intento de solución ejercicio 1

---
+++ script.js
@@ -0,0 +1,27 @@
+
+// Bill = Tenemos un array con los platos consumidos
+// K = a lo que no comió Ana
+// B = a lo que Brian le cobró a Ana
+
+ 
+
+function CalcularAna (bill, k, b){
+    let totalAna = 0;
+    for (let i =0; i < bill.length; i++) {
+        if( i !== k){
+        total += bill[i]
+        }
+    }
+
+    // Lo que tiene que pagar Ana
+    let x = Math.floor(totalAna/2);
+
+    if (x === b) {
+        console.log(CalcularAna);
+    }
+    else {
+        console.log(b- x);
+    }
+}
+
+//
