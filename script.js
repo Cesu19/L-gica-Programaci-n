f91e00f7126108fc586a06897e551f917cd9b73c
cesu19 <cesarhernancorbalan19@gmail.com>
2024-02-04 16:35:50 -0300

    Agregue comentarios y arregle errores de tipeo

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
-// Bill = Tenemos un array con los platos consumidos
-// K = a lo que no comió Ana
-// B = a lo que Brian le cobró a Ana
+// Bill = Es un array con los platos consumidos
+// k = lo que Ana no comió 
+// B = lo que Brian le cobró a Ana
 
  
 
 function CalcularAna (bill, k, b){
     let totalAna = 0;
-    for (let i =0; i < bill.length; i++) {
-        if( i !== k){
+    for (let i = 0; i < bill.length; i++) {
+        if(i !== k){
         total += bill[i]
         }
     }
@@ -17,10 +17,11 @@ function CalcularAna (bill, k, b){
     let x = Math.floor(totalAna/2);
 
     if (x === b) {
-        console.log(CalcularAna);
+        console.log("CalcularAna");
     }
     else {
         console.log(b- x);
+        // Esto sería lo que Brian tiene que devolverle a Ana
     }
 }
 
